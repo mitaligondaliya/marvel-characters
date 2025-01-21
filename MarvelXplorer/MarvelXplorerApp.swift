39a72c7f12de962e871291710626467b3a4f21b4
Mitali <gondaliyamitali19@gmail.com>
2025-01-21 11:52:59 +0530

    Initial Commit

---
+++ MarvelXplorer/MarvelXplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelXplorerApp.swift
+//  MarvelXplorer
+//
+//  Created by Mitali Gondaliya on 21/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MarvelXplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarvelXplorerTests/MarvelXplorerTests.swift
@@ -0,0 +1,16 @@
+//
+//  MarvelXplorerTests.swift
+//  MarvelXplorerTests
+//
+//  Created by Mitali Gondaliya on 21/01/25.
+//
+
+import Testing
+
+struct MarvelXplorerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

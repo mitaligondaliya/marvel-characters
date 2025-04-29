1ac465ca098de18e4d448a0e8dc74b92ef22b6c7
Mitali <gondaliyamitali19@gmail.com>
2025-04-29 23:39:24 +0530

    Added secret file

---
+++ MarvelXplorer/Dependencies/Secrets.swift
@@ -0,0 +1,14 @@
+//
+//  Secrets.swift
+//  MarvelXplorer
+//
+//  Created by Mitali Gondaliya on 29/04/25.
+//
+
+struct Secrets {
+    /// The public API key provided by Marvel for authentication.
+    static let publicKey = "fded6ad8e5190ef491d93a6f4b38b7d7"
+
+    /// The private API key provided by Marvel (should be kept secure and not exposed in production).
+    static let privateKey = "635ecd1e75ecc125a7ccecabb5fb2d61a0c3b700"
+}
